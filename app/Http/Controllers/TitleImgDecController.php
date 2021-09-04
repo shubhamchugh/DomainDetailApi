ea1fe3dd3a9d94f297e42b976cf21ff2a6262121
Shubham chugh <chugh.shubham12@gmail.com>
2021-09-04 20:06:35 +0530

    status update

--- app/Http/Controllers/TitleImgDecController.php
+++ app/Http/Controllers/TitleImgDecController.php
@@ -16,29 +16,38 @@ class TitleImgDecController extends Controller
 
         $bucket  = $request->bucket;
         $httpUrl = trim($request->url);
-        $url     = remove_http($httpUrl);
-        $dns     = new Dns($httpUrl);
 
-        try {
-            $screenshot = Browsershot::url($httpUrl)->ignoreHttpsErrors()->fit(Manipulations::FIT_CONTAIN, 460, 306)->screenshot();
-        } catch (\Throwable $th) {
-            //throw $th;
-            $screenshot = false;
-        }
+        if (!empty($httpUrl)) {
+            $url = remove_http($httpUrl);
+            $dns = new Dns($httpUrl);
 
-        if (!empty($screenshot)) {
-            $url            = Str::slug($url, '-');
-            $thumbnail_name = $url . '.png';
-            $thumbnailPath  = '/' . $thumbnail_name;
-            Storage::disk('wasabi')->put($thumbnailPath, $screenshot);
+            try {
+                $screenshot = Browsershot::url($httpUrl)->ignoreHttpsErrors()->fit(Manipulations::FIT_CONTAIN, 460, 306)->screenshot();
+            } catch (\Throwable $th) {
+                //throw $th;
+                $screenshot = false;
+            }
+
+            if (!empty($screenshot)) {
+                $url            = Str::slug($url, '-');
+                $thumbnail_name = $url . '.png';
+                $thumbnailPath  = '/' . $thumbnail_name;
+                Storage::disk('wasabi')->put($thumbnailPath, $screenshot);
+            } else {
+                $thumbnail_name = 'noimage.png';
+            }
+
+            $thumbnail['imageName'] = $thumbnail_name;
+            $seo                    = seoAnalyzer($httpUrl);
+            $final                  = array_merge($seo, $thumbnail);
+            $final['status']        = true;
+
+            return response()->json($final);
+
         } else {
-            $thumbnail_name = 'noimage.png';
+            $final['status'] = false;
+
+            return response()->json($final);
         }
-
-        $thumbnail['imageName'] = $thumbnail_name;
-        $seo                    = seoAnalyzer($httpUrl);
-        $final                  = array_merge($seo, $thumbnail);
-
-        return response()->json($final);
     }
 }
